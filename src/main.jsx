c708a7cb901df03baf316c75574c25db5ce1913d
Md Abdun Nur <mdnoyon631234@gmail.com>
2025-11-14 22:31:57 +0600

    add store file

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import { store } from "./Redux/store";
 
 import {
   createBrowserRouter,
